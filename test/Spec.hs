5ff6be2f347d09a0dc7c81af03ff68713612fcce
Szymon Stepniak <szymon.stepniak@gmail.com>
2020-03-08 21:52:06 +0100

    added primitive unit test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+import Lib
+
+assert :: Bool -> String -> String -> IO ()
+assert test passed failed = if test
+                            then putStrLn passed
+                            else putStrLn failed
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  putStrLn "Running tests..."
+  assert (isPalindrome "racecar") "passed 'racecar'" "FAIL: 'racecar'"
+  assert (isPalindrome "racecar!") "passed 'racecar!" "FAIL: 'racecar!'"
+  assert ((not . isPalindrome) "cat") "passed 'cat'" "FAIL: 'cat'"
+  putStrLn "done!"
+  
